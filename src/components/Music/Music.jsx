402746dead83e8d5d6abb454b45e4fcfddede8f3
legabog <probringer@mail.ru>
2020-05-27 19:01:58 +0300

    Add files via upload

--- src/components/Music/Music.jsx
+++ src/components/Music/Music.jsx
@@ -1,18 +1,42 @@
 import React from "react";
 import { withAuthRedirect } from "../../hoc/withAuthRedirect";
 import classes from "./Music.module.css";
-import Emoji from "../common/EmojiList/EmojiList";
-import SimpleSlider from "../common/Slider/Slider";
+import MusicPlayer from "../common/MusicPlayer/MusicPlayer";
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+
+// const useStyles = makeStyles({
+//   root: {
+//     flexGrow: 1,
+//     maxWidth: 500,
+//   },
+// });
 
 const Music = (props) => {
+  const [value, setValue] = React.useState(0);
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
   return (
-    <div className={classes.mainMusic}>
-      <audio controls />
-      <Emoji/>
-      <div style={{"width":"300px" }}>
-      <SimpleSlider/>
+    <div>
+      <MusicPlayer />
+      <div className={classes.mainMusic}>
+        <Paper square className={classes.root}>
+      <Tabs
+        value={value}
+        onChange={handleChange}
+        variant="fullWidth"
+        indicatorColor="primary"
+      >
+        <Tab label="My playlist"/>
+        <Tab label="Albums" aria-label="favorite" />
+        <Tab label="Recomendation" aria-label="person" />
+      </Tabs>
+    </Paper>
+        <div className={classes.player}></div>
       </div>
-      
     </div>
   );
 };
